22289e96c21751fe023c80e9c874979f92bfa675
Frinng <167120714+Frinng@users.noreply.github.com>
2025-04-20 17:20:38 -0300

    1.2

--- src/Livro.java
+++ src/Livro.java
@@ -4,9 +4,13 @@ public class Livro {
     private String Descricao;
     private  String isbn;
     private  boolean Capadura;
-    public void setTitulo(String titulo, String isbn, String descricao){
+    public void setTitulo(String titulo){
         this.Titulo = titulo;
+    }
+    public void setDescricao(String descricao){
         this.Descricao = descricao;
+    }
+    public void setIsbn(String isbn){
         this.isbn = isbn;
     }
     public String getTitulo() {
